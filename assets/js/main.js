c0e6c07330e5e0455b32e2009df40c779847eb34
Svetlana Shmyhol <art.firefly@gmail.com>
2021-02-12 13:13:35 +0200

    Last update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
+/*
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('active');
+	});
+});
+*/
+
 
 $(document).ready(function () {
 
-  $('.header-navbar__hamburger').on('click', function () {
+  $('.header-navbar__btn').on('click', function () {
     $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+    $('.header-navbar__list').stop(true, true).slideToggle(1000);
   });
 
 });
